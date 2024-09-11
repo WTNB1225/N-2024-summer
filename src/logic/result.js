193370c35cc48af8cb60b5e063d358214ed268b6
WTNB1225 <w.swallowsfan@gmail.com>
2024-09-11 15:49:28 +0900

    fix accuray become NaN

--- src/logic/result.js
+++ src/logic/result.js
@@ -3,17 +3,29 @@ const incorrectDom = document.getElementById('incorrect');
 const accuracyDom = document.getElementById('accuracy');
 const wpmDom = document.getElementById('wpm');
 const retryDom = document.getElementById('retry');
+const errorDom = document.getElementById('error');
+const resultDivDom = document.getElementById('result-div');
 
-const correct = window.localStorage.getItem("correct") || 0;
-const missed = window.localStorage.getItem("missed") || 0;
-const accuracy = window.localStorage.getItem("accuracy") || 0;
-const wpm = window.localStorage.getItem("wpm") || 0;
+const correct = window.localStorage.getItem("correct") || null;
+const missed = window.localStorage.getItem("missed") || null;
+const accuracy = window.localStorage.getItem("accuracy") || null;
+const wpm = window.localStorage.getItem("wpm") || null;
 
-correctDom.textContent = correct;
-incorrectDom.textContent = missed;
-accuracyDom.textContent = accuracy;
-wpmDom.textContent = wpm;
+if(correct && missed && accuracy && wpm) {
+    correctDom.textContent = correct;
+    incorrectDom.textContent = missed;
+    accuracy == "error" ? accuracyDom.textContent = "エラー" : accuracyDom.textContent = accuracy + "%";
+    wpmDom.textContent = wpm;
+} else {
+    resultDivDom.style.display = "none";
+    errorDom.textContent = "ゲームをプレイしてからリザルト画面にアクセスしてください。";
+    
+}
 
 retryDom.onclick = () => {
     window.location.href = "./game.html";
 }
+
+window.addEventListener("unload", () => {
+    window.localStorage.clear();
+});
