708cafd2b35f9afb0f1d23b2970ad615113fc034
WTNB1225 <w.swallowsfan@gmail.com>
2024-09-30 21:02:49 +0900

    add spreadsheet

--- src/logic/index.js
+++ src/logic/index.js
@@ -8,11 +8,43 @@ const promptDom = document.getElementById('prompt');
 const promptSubmitDom = document.getElementById('prompt-submit');
 const promptContainer = document.getElementById('prompt-container');
 const loadingDom = document.getElementById('loading');
+const logDom = document.getElementById('log');
 let text1 = [];
 let text2 = [];
 let remainingTime = 100;
 let countDownTime = 3;
 let parsedData;
+
+async function fetchThemeLog() {
+    const url = 'https://script.google.com/macros/s/AKfycbyG43hSvObw7CpCJHya5poZCXtrPsXRH8XSZNpxMBNRnT_2Izgk1oen8xHIggdOTw-t/exec';
+    try {
+        const response = await fetch(url);
+        if(!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error('Error:', error);
+        alert('エラーが発生しました。もう一度お試しください。');
+        window.location.href = "./game.html";
+        return;
+    }
+}
+
+fetchThemeLog().then((json) => {
+    console.log(json);
+    json.forEach((value) => { //domを生成していく
+        const log = document.createElement('button');
+        log.value = value[0];
+        log.textContent = value[0];
+        log.className = 'btn back m-2';
+        log.addEventListener('click', () => {
+            promptDom.value = log.value;
+        })
+        logDom.appendChild(log);
+    })
+});
 
 promptSubmitDom.addEventListener('click', () => {
     const prompt = `タイピングゲームで使用する文章を生成してください\nお題は${promptDom.value} です。\n条件は以下の通りです。\n 1. 句読点を含まないでください\n 2. 文章と文章の間は改行してください。\n 3. 各文章の次に、文章に対応しているすべてひらがなで構成された文章を生成してください。20個の文章を生成してください4 \n 4. 文章の先頭に数字などの余計なものはつけないでください。\n5. 生成時に了解しましたなどの言葉は不要です。本題だけを生成してください`;
@@ -39,8 +71,26 @@ async function getToken() {
 }
 
 async function generateText(prompt) {
+    logDom.classList.add('innactive');
     const tokenObj = await getToken();
-    const url = 'https://wtnbjp.com/chatgpt'; //OpenAIのAPIを叩くためのAPI 環境変数を扱うため独自のAPIを作成
+    const spreadsheetURL = 'https://script.google.com/macros/s/AKfycbxAYmfv8OsfobZrECk3tMOv3n9XCizwb-ppHkuL5oUpPAdAvNWFztfDg7AJzh4Sl4Xt/exec';
+    try {
+        const response = await fetch(spreadsheetURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: 'no-cors',
+            body: JSON.stringify({theme: promptDom.value}),
+        });
+    } catch (error) {
+        console.error('Error:', error);
+        console.log(response);
+        alert('エラーが発生しました。もう一度お試しください。');
+        window.location.href = "./game.html";
+        return;
+    }
+    const url = 'https://wtnbjp.com/chatgpt'; //OpenAIのAPIを叩くためcdのAPI 環境変数を扱うため独自のAPIを作成
     try {
         const response = await fetch(url, {
             method: 'POST',
@@ -77,6 +127,7 @@ function activeCountDown() {
     loadingDom.classList.add('innactive');
     remainingTimeDom.classList.remove('innactive');
     sentenceContainer.classList.remove('innactive');
+    //logDom.classList.add('innactive');
     const countDown = setInterval(() => {
         countDownTime -= 1;
         remainingTimeDom.textContent = countDownTime;
